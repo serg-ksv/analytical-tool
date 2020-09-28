4cdcb8d818c6c723b83558f9fd758bea90142b7f
serg-ksv <serg.ksv1@gmail.com>
2020-09-28 15:55:31 +0300

    first commit

---
+++ src/main/java/com/ksv/analyticaltool/AnalyticalToolApplication.java
@@ -0,0 +1,13 @@
+package com.ksv.analyticaltool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticalToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticalToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksv/analyticaltool/AnalyticalToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksv.analyticaltool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticalToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
